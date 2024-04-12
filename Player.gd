46ec1235e86e6e4c26a29b7b1a60968d8d02ae5c
Ethel Arterberry <8540373+soops@users.noreply.github.com>
2024-04-11 18:38:26 -0700

    New animations, etc

--- Player.gd
+++ Player.gd
@@ -11,14 +11,25 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	var motion = Vector2()
 
+	$AnimatedSprite2D.play()
+	
 	if Input.is_action_pressed("ui_up"):
 		motion.y -= 1
-	if Input.is_action_pressed("ui_down"):
+		$AnimatedSprite2D.animation = "up"
+	elif Input.is_action_pressed("ui_down"):
 		motion.y += 1
-	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.animation = "down"
+	elif Input.is_action_pressed("ui_left"):
 		motion.x -= 1
-	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.animation = "left"
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("ui_right"):
 		motion.x += 1
+		$AnimatedSprite2D.animation = "left"
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.frame = 0
+		$AnimatedSprite2D.stop()
 	
 	velocity = motion.normalized() * SPEED
 	
